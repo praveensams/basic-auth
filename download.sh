c19da01de6453ed6354a14e89b476298429ef352
praveen <praveensams16@gmail.com>
2019-10-01 10:43:51 +0000

    uploads

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ times=$(date +%H%M)
 
 [ -d /sftp/logs/${dates}/${times} ] || mkdir -p /sftp/logs/${dates}/${times}
 
-for app in prod-communication-sms-v1  prod-home-delivery-process-v1  prod-online-order-processing-v1  prod-payment-processing-v1
+for app in prod-communication-sms-v1  prod-home-delivery-process-v1  prod-online-order-processing-v1  prod-payment-processing-v1  prod-order-tracking-process-v1
 do
 	docker run --rm -v /sftp/logs/${dates}/${times}:/mnt -it integrational/anypoint-cli:3.0.0 --username=praveensams16 --password=Samhome@162 runtime-mgr cloudhub-application download-logs $app /mnt
 done
